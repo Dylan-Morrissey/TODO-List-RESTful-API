1cc8681dfa033fdcd5fb3626dc92cb3b09058025
Dylan-Morrissey <dylanmorrissey2010@gmail.com>
2018-10-02 17:51:11 +0100

    Added Parsing and handling CORS

--- app.js
+++ app.js
@@ -10,6 +10,16 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.header('Acess-Control-Allow-Origin', '*');
+    res.header('Acess-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
+
 app.use('/todo', todoRoutes);
 app.use('/list', listRoutes);
 
