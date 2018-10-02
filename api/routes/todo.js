70fce8971b0049de7f04e19232e66169edbdf2ba
Dylan-Morrissey <dylanmorrissey2010@gmail.com>
2018-10-03 00:16:22 +0100

    Added to mongoDB Atlas updated all the methods for TODO to update the DB

--- api/routes/todo.js
+++ api/routes/todo.js
@@ -1,49 +1,71 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+
+const Todo = require('../models/todo');
 
 router.get('/', (req, res ,next) => {
-    res.status(200).json({
-            message: 'Handling GET requests to /todo'
-        }
-    )
+    Todo.find().find()
+        .exec()
+        .then(docs => {
+            console.log(docs);
+            res.status(200).json(docs);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            })
+        });
 });
 
 router.post('/', (req, res ,next) => {
-    const todo = {
+    const todo = new Todo({
+        _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
-        activity: req.body.activity
-    };
-    res.status(201).json({
-            message: 'Handling POST requests to /todo',
-        createdProduct: todo
-        }
-    )
+        task: req.body.task
+    });
+    todo.save().then(result => {
+        console.log(result);
+    }).catch(err => {console.log(err);res.status(500).json({error: err})});
+
 });
 
 router.get('/:todoId', (req, res, next) => {
     const id = req.params.todoId;
-    if (id === 'special') {
-        res.status(200).json({
-            message: 'You Discovered the special ID',
-            id: id
-        });
-    }else {
-        res.status(200).json({
-            message: 'You Passed an ID'
-        });
-    }
+    Todo.findById(id).exec()
+        .then(doc => {console.log(doc);
+        if (doc) {
+            res.status(200).json(doc);
+        } else {
+            res.status(404).json({message: 'No Valid Entry Found'})
+        }
+
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json({error: err});
+    });
 });
 
 router.patch('/:todoId', (req, res, next) => {
-   res.status(200).json({
-       message:'Updated todo'
-   });
+    const id = req.params.todoId;
+    const updateOps = {};
+    for (const ops of req.body) {
+        updateOps[ops.propName] = ops.value
+    }
+    Todo.update({_id: id}, {$set: {name: req.body.newName, task: req.body.newTask }});
 });
 
 router.delete('/:todoId', (req, res, next) => {
-    res.status(200).json({
-        message:'Deleted todo'
-    });
+    const id = req.params.todoId;
+    Todo.remove({_id: id})
+        .exec()
+        .then(res => {
+            res.status(200).json(result);
+        })
+        .catch(err => {
+            console.log(err);
+        });
 });
 
 module.exports = router;
